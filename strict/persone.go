8eea454346ccf848b7ccc5c66e4c2686d59419e4
figurecode <figurecode@gmail.com>
2024-02-09 17:07:17 +0300

    chore: проба описать экспортируемый тип

---
+++ strict/persone.go
@@ -0,0 +1,19 @@
+package person
+
+import (
+	"time"
+)
+
+type Persone struct {
+	Name        string
+	Email       string
+	dateOfBirth time.Time
+}
+
+func NewPersone(name, email string, dobYear, dobMonth, dobDay int) Persone {
+	return Persone{
+		Name:        name,
+		Email:       email,
+		dateOfBirth: time.Date(dobYear, time.Month(dobMonth), dobDay, 0, 0, 0, 0, time.UTC),
+	}
+}
